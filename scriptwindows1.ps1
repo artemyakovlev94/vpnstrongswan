12c85382175f363270ab29b4d13382d046b587ee
artemyakovlev94 <99836360+artemyakovlev94@users.noreply.github.com>
2022-03-12 19:19:13 +0500

    Create scriptwindows1.ps1

---
+++ scriptwindows1.ps1
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+$CAContent = "$( cat /etc/ipsec.d/cacerts/ca.pem )"
+$HostName = "HOST_NAME"
+$HostAddress = "HOST_IP_ADDRESS"
+
+try {
+    New-Item -Path . -Name "ca.pem" -ItemType "file" -Value $CAContent -Force
+}
+catch {
+    Write-Error "An error occurred:"
+    Write-Error $_
+    Read-Host "Для продолжения нажмите клавишу ENTER ..."
+    Break
+}
+
+try { 
+    Import-Certificate -CertStoreLocation cert:\LocalMachine\Root\ -FilePath ca.pem
+    Write-Host "Сертификат центра сертификации VPN сервера успешно установлен" -ForegroundColor Green
+}
+catch {
+    Remove-item ca.pem
+    Write-Error "An error occurred:"
+    Write-Error $_
+    Break
+}
+
+Remove-item ca.pem
+
+Remove-VpnConnection -Name $HostName -Force
+
+Add-VpnConnection -Name $HostName -ServerAddress $HostAddress -TunnelType "IKEv2" -AuthenticationMethod "EAP" -EncryptionLevel "Maximum" -RememberCredential `
+
+Set-VpnConnectionIPsecConfiguration -Name $HostName -AuthenticationTransformConstants GCMAES256 -CipherTransformConstants GCMAES256 -DHGroup ECP384 -IntegrityCheckMethod SHA384 -PfsGroup ECP384 -EncryptionMethod GCMAES256
+
+Get-VpnConnection -Name $HostName
+
+Read-Host "Для продолжения нажмите клавишу ENTER ..."
